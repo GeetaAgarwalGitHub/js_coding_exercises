7d0cf3a612786eb9b97e4426255a361556119ac1
Vijay Agarwal <AGARWAV@domesticandgeneral.com>
2022-02-09 13:51:54 +0000

    Update exercise006.js
    Implemented functions for exercise 006

--- challenges/exercise006.js
+++ challenges/exercise006.js
@@ -6,6 +6,29 @@
  */
 const sumMultiples = arr => {
   if (arr === undefined) throw new Error("arr is required");
+
+
+  //varaibles to store the remainders when number divided by 3 & 5
+  var isDivisibleBy3;
+  var isDivisibleBy5;
+
+  //variable to store the sum of numbers divisible by 3 or 5
+  var totalSum = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    //store the remainders when number divided by 3 & 5
+    isDivisibleBy3 = arr[i] % 3;
+    isDivisibleBy5 = arr[i] % 5;
+
+
+    //if remainder is 0 add to the total
+    if (isDivisibleBy3 == 0 || isDivisibleBy5 == 0)
+      totalSum = totalSum + arr[i];
+
+
+  }
+  //return total
+  return totalSum;
 };
 
 /**
@@ -15,6 +38,21 @@ const sumMultiples = arr => {
  */
 const isValidDNA = str => {
   if (str === undefined) throw new Error("str is required");
+
+  //variable to convert the string to lowercase
+  var DNA = str.toString().toLowerCase();
+
+  //iterate through each char in string
+  for (var i = 0; i < DNA.length; i++) {
+
+    //IF not A, C, G, T return false
+    if (!DNA[i].includes('c') && !DNA[i].includes('g') && !DNA[i].includes('t') && !DNA[i].includes('a'))
+      return false;
+  }
+
+  //else return true
+  return true;
+
 };
 
 /**
@@ -24,6 +62,27 @@ const isValidDNA = str => {
  */
 const getComplementaryDNA = str => {
   if (str === undefined) throw new Error("str is required");
+
+  if (!isValidDNA(str))
+    return ('Invalid DNA!!');
+
+  //object saves the pair as key value
+  var DNAObject = {
+    T: "A",
+    C: "G",
+    A: "T",
+    G: "C"
+  }
+
+  //variable to store the result
+  var resultBaseDNA = '';
+
+  //iterate through each character in string and get value based on key in DNA object
+  for (var i = 0; i < str.length; i++) {
+    resultBaseDNA = resultBaseDNA + DNAObject[str[i].toUpperCase()];
+
+  }
+  return resultBaseDNA;
 };
 
 /**
@@ -33,7 +92,25 @@ const getComplementaryDNA = str => {
  */
 const isItPrime = n => {
   if (n === undefined) throw new Error("n is required");
+
+  //return false if negative number
+  if (n <= 1) return false;
+
+  //if even number then not prime
+  if (n % 2 == 0 && n > 2)
+    return false;
+
+  // store the square to loop faster
+  const s = Math.sqrt(n);
+  //iterate through each number from 3 to square root
+  for (let i = 3; i <= s; i += 2) {
+    //if remainder is 0 means not primt
+    if (n % i === 0)
+      return false;
+  }
+  return true;
 };
+
 
 /**
  * This function should receive a number and return an array of n arrays, each filled with n items. The parameter "fill" should be used as the filler of the arrays. For example, given parameters 3 and "foo" the resulting matrix should be:
@@ -49,6 +126,22 @@ const isItPrime = n => {
 const createMatrix = (n, fill) => {
   if (n === undefined) throw new Error("n is required");
   if (fill === undefined) throw new Error("fill is required");
+
+  var ResultArray = new Array();
+  var subArray = new Array();
+
+  //iterate 0 through n to creat arrays
+  for (i = 0; i < n; i++) {
+
+    //fill the subarrays with content
+
+    for (j = 0; j < n; j++)
+      subArray[j] = fill;
+
+    //add subarray to result
+    ResultArray[i] = subArray;
+  }
+  return ResultArray;
 };
 
 /**
@@ -66,6 +159,25 @@ const createMatrix = (n, fill) => {
 const areWeCovered = (staff, day) => {
   if (staff === undefined) throw new Error("staff is required");
   if (day === undefined) throw new Error("day is required");
+
+  //variable to save the number of staff
+  var numberOfStaff = 0;
+
+  //iterate through each staff
+  for (var i = 0; i < staff.length; i++) {
+    //if the rota contains the required day, increment counter
+    if (staff[i]['rota'].toString().toLowerCase().includes(day.toLowerCase()))
+      numberOfStaff++;
+
+  }
+
+  //if number of staff are less than 3 return false
+  if (numberOfStaff >= 3)
+    return true;
+
+  //else return true
+  else
+    return false;
 };
 
 module.exports = {
