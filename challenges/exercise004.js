ade28437d5126f72fa742fc567c795f21020adb0
Vijay Agarwal <AGARWAV@domesticandgeneral.com>
2022-02-15 00:32:11 +0000

    Update exercise004.js
    Implemented map and Max features in getLongestSides

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -9,14 +9,14 @@ function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
 
-  return names.filter(name => name.substring(0,1).toLowerCase().includes(char.toString().toLowerCase()))
- 
+  return names.filter(name => name.substring(0, 1).toLowerCase().includes(char.toString().toLowerCase()))
+
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
 
-  return words.filter(word => word.substring(0,3).toLowerCase().includes('to '))
+  return words.filter(word => word.substring(0, 3).toLowerCase().includes('to '))
 
 }
 
@@ -43,58 +43,21 @@ function getCities(users) {
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
 
-  return nums.map(num => Math.round(Math.sqrt(num)* 100) / 100)
+  return nums.map(num => Math.round(Math.sqrt(num) * 100) / 100)
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
 
-return sentences.filter(sentence => sentence.toLowerCase().includes(str.toLowerCase()))
-/*
-  //variable to store the result array
-  var resultArray = new Array();
-  var index = 0;
+  return sentences.filter(sentence => sentence.toLowerCase().includes(str.toLowerCase()))
 
-  //iterate through each sentence in array
-  for (let i = 0; i < sentences.length; i++) {
-
-    //if sentencehas required string
-    if (sentences[i].toLowerCase().includes(str.toLowerCase())) {
-
-      //add result to array
-      resultArray[index] = sentences[i];
-      index++;
-    }
-  }
-  return resultArray;*/
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
 
-
-
-  //variable to store the result array
-  var resultArray = new Array();
-  var index = 0;
-  var currLongerSide = 0;
-
-  //iterate through each traingle in array
-  for (let i = 0; i < triangles.length; i++) {
-    //iterate through each length for the triangle
-    for (let j = 0; j < 3; j++) {
-      //if current length is longer then save it
-      if (triangles[i][j] > currLongerSide)
-        currLongerSide = triangles[i][j];
-    }
-
-    //add longer side to result array
-    resultArray[index] = currLongerSide;
-    currLongerSide = 0;
-    index++;
-  }
-  return resultArray;
+  return triangles.map(sides => Math.max(...sides))
 }
 
 module.exports = {
